448f4be241a808bbf4a6f9daf37a3e868784e777
v1ll4n <v1ll4n@qq.com>
2022-01-07 16:34:09 +0800

    add yakit-arm64

---
+++ Casks/yakit-arm64.rb
@@ -0,0 +1,8 @@
+cask "yakit-arm64" do 
+    url "https://yaklang.oss-cn-beijing.aliyuncs.com/yak/1.0.11-sp13/Yakit-1.0.11-sp13-darwin-arm64.dmg"
+    name "Yakit"
+    desc "Yakit gRPC client for yaks engine"
+    homepage "https://yaklang.io"
+
+    app "Yakit.app"
+end
