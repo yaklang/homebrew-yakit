92598011ad6c4d1bb23932dea35b9cce963c4bf7
v1ll4n <v1ll4n@qq.com>
2022-01-07 17:02:22 +0800

    use oss-aliyunaccelerate

--- Casks/yakit-arm64.rb
+++ Casks/yakit-arm64.rb
@@ -1,5 +1,5 @@
 cask "yakit-arm64" do 
-    url "https://yaklang.oss-cn-beijing.aliyuncs.com/yak/1.0.11-sp13/Yakit-1.0.11-sp13-darwin-arm64.dmg"
+    url "https://yaklang.oss-accelerate.aliyuncs.com/yak/1.0.11-sp13/Yakit-1.0.11-sp13-darwin-arm64.dmg"
     name "Yakit"
     desc "Yakit gRPC client for yaks engine"
     homepage "https://yaklang.io"
--- Casks/yakit-x64.rb
+++ Casks/yakit-x64.rb
@@ -1,5 +1,5 @@
 cask "yakit-x64" do 
-    url "https://yaklang.oss-cn-beijing.aliyuncs.com/yak/1.0.11-sp13/Yakit-1.0.11-sp13-darwin-x64.dmg"
+    url "https://yaklang.oss-accelerate.aliyuncs.com/yak/1.0.11-sp13/Yakit-1.0.11-sp13-darwin-x64.dmg"
     name "Yakit"
     desc "Yakit gRPC client for yaks engine"
     homepage "https://yaklang.io"
